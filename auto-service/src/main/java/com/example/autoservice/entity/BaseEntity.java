ebe22c31d6b147380eb70e3a88f7d1020e14b572
Vladimir Krivov <vov.krv@yandex.ru>
2023-07-01 15:30:39 +0300

    Init project

---
+++ auto-service/src/main/java/com/example/autoservice/entity/BaseEntity.java
@@ -0,0 +1,48 @@
+package com.example.autoservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.io.Serializable;
+import java.time.OffsetDateTime;
+import java.util.Calendar;
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
+    private OffsetDateTime createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "deleted_at")
+    private OffsetDateTime deletedAt;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive = true;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ auto-service/src/main/java/com/example/autoservice/grpc/AutoServiceGrpc.java
@@ -0,0 +1,47 @@
+package com.example.autoservice.grpc;
+
+import auto.AutoServiceOuterClass;
+import com.example.autoservice.entity.Auto;
+import com.example.autoservice.repository.AutoRepository;
+import com.example.autoservice.service.AutoService;
+import io.grpc.stub.StreamObserver;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+import java.time.OffsetDateTime;
+
+@Slf4j
+@GrpcService
+@RequiredArgsConstructor
+public class AutoServiceGrpc extends auto.AutoServiceGrpc.AutoServiceImplBase {
+
+    private final AutoService autoService;
+
+    @Override
+    @Transactional
+    public void createAuto(AutoServiceOuterClass.AutoRequest request,
+                           StreamObserver<AutoServiceOuterClass.AutoResponse> responseObserver) {
+        responseObserver.onNext(autoService.createAuto(request));
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getAutoById(AutoServiceOuterClass.Id request,
+                            StreamObserver<AutoServiceOuterClass.AutoResponse> responseObserver) {
+        super.getAutoById(request, responseObserver);
+    }
+
+    @Override
+    public void updateAuto(AutoServiceOuterClass.Id request,
+                           StreamObserver<AutoServiceOuterClass.AutoResponse> responseObserver) {
+        super.updateAuto(request, responseObserver);
+    }
+
+    @Override
+    public void deleteAuto(AutoServiceOuterClass.Id request,
+                           StreamObserver<AutoServiceOuterClass.DeletedResponse> responseObserver) {
+        super.deleteAuto(request, responseObserver);
+    }
+}
